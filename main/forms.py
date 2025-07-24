bc2d5281951d5051ebb3c1ae3b6434dc960a3e7a
LucLor06 <lukelorenzba@gmail.com>
2025-07-24 17:04:36 -0500

    fix(forms): Fix improper saving order in RegisterForm save method

--- main/forms.py
+++ main/forms.py
@@ -12,6 +12,6 @@ class RegisterForm(UserCreationForm):
         self.fields['password1'].help_text = 'Minimum 8 characters. Must include letters and numbers.'
 
     def save(self, commit=True):
+        user = super().save(commit)
         if commit:
-            self.instance.send_verification_email()
-        return super().save(commit)
+            user.send_verification_email()
