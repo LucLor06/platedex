d02e55254ede9d71557140a35a9c37ff1962640f
LucLor06 <lukelorenzba@gmail.com>
2025-07-24 16:16:26 -0500

    feat(views): Add accounts_register view

--- main/views.py
+++ main/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpRequest
+from .forms import RegisterForm
+from django.shortcuts import redirect
 
-def home(request):
+def home(request: HttpRequest):
     return render(request, 'home.html')
+
+def accounts_register(request:HttpRequest):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('register')
+    else:
+        form = RegisterForm()
+    context = {'form': form}
+    return render(request, 'accounts/register.html', context)
