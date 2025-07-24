37619950b776708771e2a0d3f0445676c4624878
LucLor06 <lukelorenzba@gmail.com>
2025-07-23 22:55:12 -0500

    feat(main.models): Add LicensePlate model
    The LicensePlate model is a model for representing a US license plate. The model simply contains the plate number and a field for tracking how many times the plate has been searched. Instances will validate on creation.

--- main/models.py
+++ main/models.py
@@ -1,7 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinLengthValidator
+
 
 class User(AbstractUser):
     email = models.EmailField()
 
     REQUIRED_FIELDS = ['email']
+
+
+class LicensePlate(models.Model):
+    number = models.CharField(max_length=7, validators=[MinLengthValidator(2)])
+    views = models.PositiveBigIntegerField(default=0)
+
+    def __str__(self):
+        return self.number
+    
+    def save(self, *args, **kwargs):
+        if self._state.adding:
+            self.full_clean()
+        return super().save(*args, **kwargs)
