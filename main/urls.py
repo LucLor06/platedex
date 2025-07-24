0c99861dd5b5101c14cc039825eb3ee16e6dfca0
LucLor06 <lukelorenzba@gmail.com>
2025-07-24 17:44:29 -0500

    feat(urls): Add accounts-email-verify url path

--- main/urls.py
+++ main/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('accounts/register/', views.accounts_register, name='accounts-register'),
+    path('accounts/email/verify/', views.accounts_email_verify, name='accounts-email-verify'),
     path('accounts/email/verify/done/', views.accounts_email_verify_done, name='accounts-email-verify-done'),
-    path('accounts/email/verify/<uidb64>/<token>/', views.accounts_email_verify_confirm, name='accounts-email-verify-confirm')
+    path('accounts/email/verify/<uidb64>/<token>/', views.accounts_email_verify_confirm, name='accounts-email-verify-confirm'),
 ]
