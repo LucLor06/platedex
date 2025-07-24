01ab8f6bdd8fae604ccd8daf200daae6c299a650
LucLor06 <lukelorenzba@gmail.com>
2025-07-24 16:44:24 -0500

    feat(urls): Add accounts-register url path

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('accounts/register/', views.accounts_register, name='accounts-register')
 ]
