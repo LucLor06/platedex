60dba22f5cc9e3d0e58244a037a56738135e3459
LucLor06 <lukelorenzba@gmail.com>
2025-07-24 16:08:04 -0500

    feat(forms): Add RegisterForm form

---
+++ main/forms.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.forms import UserCreationForm
+from .models import User
+
+
+class RegisterForm(UserCreationForm):
+    class Meta:
+        models = User
+        fields = ['username', 'email']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['password1'].help_text = 'Minimum 8 characters. Must include letters and numbers.'
+
+    def save(self, commit=True):
+        if commit:
+            form.instance.send_verification_email()
+        return super().save(commit)
