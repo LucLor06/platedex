c7d66bc9ed4fd562f886cbd8b945a4ca923d4f00
LucLor06 <lukelorenzba@gmail.com>
2025-07-25 10:11:31 -0500

    feat(urls): Add accounts-password-reset-confirm url path

--- main/urls.py
+++ main/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('accounts/email/verify/done/', views.accounts_email_verify_done, name='accounts-email-verify-done'),
     path('accounts/email/verify/<uidb64>/<token>/', views.accounts_email_verify_confirm, name='accounts-email-verify-confirm'),
     path('accounts/password/reset/', views.AccountsPasswordResetView.as_view(), name='accounts-password-reset'),
-    path('accounts/password/reset/done/', views.AccountsPasswordResetDoneView.as_view(), name='accounts-password-reset-done')
+    path('accounts/password/reset/done/', views.AccountsPasswordResetDoneView.as_view(), name='accounts-password-reset-done'),
+    path('accounts/password/reset/confirm/', views.AccountsPasswordResetConfirmView.as_view(), name='accounts-password-reset-confirm'),
 ]
