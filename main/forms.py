1420661b4963aa1a99f52d46a9f1563142c20ef8
LucLor06 <LukeLorenzBA@gmail.com>
2025-07-29 23:14:22 -0400

    feat(forms): Add LicensePlateImageValidationForm form

--- main/forms.py
+++ main/forms.py
@@ -1,7 +1,11 @@
 from django.contrib.auth.forms import UserCreationForm, SetPasswordForm
 from .models import User, LicensePlate
 from django import forms
+import easyocr
+from django.core.exceptions import ValidationError
+from .openai_api import license_plate_image_is_valid
 
+reader = easyocr.Reader(['en'], gpu=False)
 
 class RegisterForm(UserCreationForm):
     class Meta:
@@ -40,3 +44,24 @@ class LicensePlateNumberForm(forms.ModelForm):
     class Meta:
         model = LicensePlate
         fields = ['number']
+
+    def clean_number(self):
+        data = self.cleaned_data['number']
+        return data.upper()
+
+class LicensePlateImageValidationForm(forms.ModelForm):
+    image = forms.ImageField(required=True)
+
+    class Meta:
+        model = LicensePlate
+        fields = ['image']
+
+    def clean_image(self):
+        data = self.cleaned_data['image'].read()
+        ocr_output = reader.readtext(data, detail=0)
+        if isinstance(ocr_output, list):
+            ocr_output = ''.join(ocr_output)
+        is_valid = license_plate_image_is_valid(self.instance, ocr_output)
+        if not is_valid:
+            raise ValidationError('The text in the image does not match the plate number. Ensure the picture is not too blurry, and does not contain any text in te background')
+        return is_valid
